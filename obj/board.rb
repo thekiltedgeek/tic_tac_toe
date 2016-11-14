9752966365d75d7ac1a650f0650125782b0990fe
Larry Shanahan <thekiltedgeek@gmail.com>
2016-11-14 10:39:48 -0500

    code cleanup on Board class

--- obj/board.rb
+++ obj/board.rb
@@ -2,25 +2,19 @@
 #class for the game board
 
 class Board
-    attr_reader :board
+    
     def initialize
         initializeBoard
     end
     
     def row(index)
-        return @board[index]
-    end
-    
-    def printRow(index)
-        rowstring = ''
-        self.row(index).each {|cel| rowstring << "#{cel} "}
-        return rowstring
+        return @grid[index]
     end
     
     def column(index)
         cols = []
         (0..2).each do |i|
-            cols << self[i][index]
+            cols << @grid[i][index]
         end
         
         return cols
@@ -35,25 +29,31 @@ class Board
     end
     
     def move(row,col,mark)
-        @board[row - 1][col - 1] = mark if is_legal?(row - 1,col - 1)
+        @grid[row - 1][col - 1] = mark if is_legal?(row - 1,col - 1)
     end
     
     private
     
+    def printRow(index)
+        rowstring = ''
+        self.row(index).each {|cel| rowstring << "#{cel} "}
+        return rowstring
+    end
+    
     def is_legal? (row,col)
         if (0..2) === row && (0..2) === col
-            return @board[row][col] == '-'
+            return @grid[row][col] == '-'
         else
             return false
         end
     end
     
     def win? (col, row, mark)
-        return @board.row(row).all? {|cel| cel == mark} || @board.column(col).all?  {|cel| cel == mark}
+        return self.row(row).all? {|cel| cel == mark} || self.column(col).all?  {|cel| cel == mark}
     end
     
     def initializeBoard
-        @board = Array.new
-        (0..2).each {@board << ['-','-','-']}
+        @grid = Array.new
+        (0..2).each {@grid << ['-','-','-']}
     end
 end
